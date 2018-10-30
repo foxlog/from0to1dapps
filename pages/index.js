7fe1d0980e9a3a0ac5b915985392b3b119b641c6
Alex Wang <idea.wangtj@gmail.com>
2018-10-30 11:39:53 +0800

    集成 Material UI

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import withRoot from '../libs/withRoot';
 
-export default class Index extends React.Component {
+class Index extends React.Component {
   render() {
       return <Button variant="raised" color="primary">Welcome to Ethereum ICO DApp!</Button>;
   }
