168616c6d9694643baffb2580cdcf90ab579d1ef
Alex Wang <idea.wangtj@gmail.com>
2018-11-03 12:19:55 +0800

    项目列表优化显示, summary 之index.js前端数据获取与渲染部分.

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,75 @@
 import React from 'react';
 import {Link} from '../routes';
 import web3 from '../libs/web3';
+import Project from '../libs/project';
 import ProjectList from '../libs/projectList';
-import { Grid,  Button, Typography, Card, CardContent, CardActions } from '@material-ui/core';
+import {Grid, Button, Typography, Card, CardContent, CardActions} from '@material-ui/core';
 import withRoot from '../libs/withRoot';
 import Layout from "../components/Layout";
 
 class Index extends React.Component {
     static async getInitialProps({req}) {
-        const projects = await ProjectList.methods.getProjects().call();
+        //在这里修改summary, 数据获取部分
+        const addressList = await ProjectList.methods.getProjects().call();
+        const summaryList = await Promise.all(
+            addressList.map(address =>
+                Project(address)
+                    .methods.getSummary()
+                    .call())
+        );
+
+        console.log({summaryList});
+        const projects = addressList.map((address, i) => {
+            const [description, minInvest, maxInvest, goal, balance, investorCount, paymentCount, owner] = Object.values(summaryList[i]);
+            return {
+                address,
+                description,
+                minInvest,
+                maxInvest,
+                goal,
+                balance,
+                investorCount,
+                paymentCount,
+                owner,
+            };
+        });
+
+        console.log(projects);
 
         return {projects};
+
+
     }
 
 
-  render() {
-      const {projects} = this.props;
+    render() {
+        const {projects} = this.props;
 
         return (
             <Grid container spacing={16}>
                 {projects.map(this.renderProject)}
             </Grid>
         );
-  }
+    }
 
+    //数据渲染部分
     renderProject(project) {
         return (
-            <Grid item md={4} key={project}>
+            <Grid item md={4} key={project.address}>
                 <Card>
                     <CardContent>
                         <Typography gutterBottom variant="headline" component="h2">
-                            {project}
+                            {project.description}
                         </Typography>
-                        <Typography component="p"> {project}</Typography>
+                        <Typography component="p"> {project.address}</Typography>
                     </CardContent>
                     <CardActions>
-                        <Link route={`/projects/${project}`}>
+                        <Link route={`/projects/${project.address}`}>
                             <Button size="small" color="primary">
                                 立即投资
                             </Button>
                         </Link>
-                        <Link route={`projects/${project}`}>
+                        <Link route={`projects/${project.address}`}>
                             <Button size="small" color="secondary">
                                 查看详情
                             </Button>
